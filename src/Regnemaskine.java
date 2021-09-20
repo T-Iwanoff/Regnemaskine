301b6420b0f8ecf45be98d8bc7adbd6243acdb9b
Thor Jensen <thor@iwanoff.dk>
2021-09-20 16:01:42 +0200

    Initial commit

---
+++ src/Regnemaskine.java
@@ -0,0 +1,45 @@
+public class Regnemaskine {
+    public static void main(String[] args) {
+        double a, b;
+        String method, again;
+        boolean repeat = true;
+
+
+        while(repeat) {
+            System.out.println("Do you want to add, subtract, multiply or divide?");
+            java.util.Scanner brugerinput1 = new java.util.Scanner(System.in);
+            method = brugerinput1.next();
+
+            System.out.println("First number:");
+            java.util.Scanner brugerinput2 = new java.util.Scanner(System.in);
+            a = brugerinput2.nextInt();
+
+            System.out.println("Second number:");
+            java.util.Scanner brugerinput3 = new java.util.Scanner(System.in);
+            b = brugerinput3.nextInt();
+
+            if (method.equals("add")) {
+                System.out.println(a + b);
+            }
+            if (method.equals("subtract")) {
+                System.out.println(a - b);
+            }
+            if (method.equals("multiply")) {
+                System.out.println(a * b);
+            }
+            if (method.equals("divide")) {
+                System.out.println(a / b);
+            }
+
+            System.out.println("Try again?");
+            java.util.Scanner brugerinput4 = new java.util.Scanner(System.in);
+            again = brugerinput4.next();
+            if (again.equals("yes")) {
+            }
+            else {
+                repeat = false;
+                System.out.println("Goodbye.");
+            }
+        }
+    }
+}
